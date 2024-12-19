b349ff554f2e690f1bf1f70093331565d559b06d
Tsion Tamirat <tayu.16.enye@gmail.com>
2024-12-19 22:39:25 +0300

    added functionality to prevent tenant from creating more than ones within the same buildings

--- BM/Controllers/BuildingTenantsController.cs
+++ BM/Controllers/BuildingTenantsController.cs
@@ -41,7 +41,9 @@ namespace BM.Controllers
             var tenants = _context.Tenants
                 .Include(x => x.Building)
                 .Include(x => x.TenantType)
-                .Where(x => buildingId.Contains(x.BuildingId)); 
+                 .Include(x => x.TenantUsers)
+
+                .Where(x => buildingId.Contains(x.BuildingId) && x.TenantUsers.Any(tu => tu.UserId == userId)).ToList(); 
 
             ViewData["BuildingId"] = new SelectList(_context.Buildings
                 .Where(b => buildingId.Contains(b.Id)), "Id", "Name"); 
@@ -49,76 +51,6 @@ namespace BM.Controllers
 
             return View(tenants);
         }
-
-
-        //public IActionResult Index(int? id)
-        //{
-
-        //    int? userId = HttpContext.Session.GetInt32("UserId");
-
-        //    if (!userId.HasValue)
-        //    {
-        //        return RedirectToAction("Login", "Account");
-        //    }
-
-        //    var approvedRoomRentalRequests = _context.RoomRentalRequests
-        //       .Where(rr => rr.UserId == userId && rr.RequestStatusId == 2)
-        //       .Select(rr => rr.RoomId);
-        //    ViewBag.ApprovedRoomRentalRequests = approvedRoomRentalRequests;
-        //    var tenant = _context.Tenants
-        //        .Include(x => x.Building)
-        //        //.Include(x => x.Floors).ThenInclude(x => x.Rooms)
-        //        //.Include(x => x.City)
-        //        .Include(x => x.TenantType)
-        //        .Where(x => x.BuildingId == id);
-        //    ViewData["BuildingId"] = new SelectList(_context.Buildings, "Id", "Name");
-        //    ViewData["TenantTypeId"] = new SelectList(_context.TenantTypes, "Id", "Name");
-        //    return View(tenant);
-        //}
-
-        //.FirstOrDefault(x => x.Id == id);
-
-        //if (id == null)
-        //{
-        //    return NotFound();
-        //}
-
-
-        //var building = _context.Buildings
-        //    .Include(x => x.BuildingType)
-        //    .Include(x => x.Floors).ThenInclude(x => x.Rooms)
-        //    .Include(x => x.City)
-        //    .Include(x => x.Tenants).ThenInclude(x => x.TenantType)
-        //    .FirstOrDefault(x => x.Id == id); 
-
-        //if (building == null)
-        //{
-        //    return NotFound();
-        //}
-
-
-        //int? userId = HttpContext.Session.GetInt32("UserId");
-        //if (!userId.HasValue)
-        //{
-        //    return RedirectToAction("Login", "Account");
-        //}
-
-
-        //var approvedRoomRentalRequests = _context.RoomRentalRequests
-        //    .Where(rr => rr.UserId == userId && rr.RequestStatusId == 2)
-        //    .Select(rr => rr.RoomId);
-
-
-        //var rooms = _context.Rooms
-        //    .Include(x => x.Floor).ThenInclude(x => x.Building).ThenInclude(x => x.City)
-        //    .Where(x => x.IsActive && approvedRoomRentalRequests.Contains(x.Id))
-        //    .Select(s => new
-        //    {
-        //        s.Id,
-        //        Name = $"{s.Name}/{s.Floor.Name}/{s.Floor.Building.Name}/{s.Floor.Building.City.Name}"
-        //    }).ToList(); 
-
-
 
 
         public IActionResult Details(int? id)
@@ -197,10 +129,14 @@ namespace BM.Controllers
             {
                 return BadRequest("Invalid building ID.");
             }
+
             int? userId = HttpContext.Session.GetInt32("UserId");
 
-            var existingtenant = _context.TenantUsers.Where(x => x.UserId == userId);
-            if (existingtenant == null)
+            var existingTenant = _context.TenantUsers
+                .Include(x => x.Tenant)
+                .Any(x => x.UserId == userId && x.Tenant.BuildingId == BuildingId);
+
+            if (!existingTenant) 
             {
                 var tenant = new Tenant
                 {
@@ -216,18 +152,16 @@ namespace BM.Controllers
                 _context.Tenants.Add(tenant);
                 _context.SaveChanges();
 
-
                 CreateTenantUser(tenant.Id, userId.Value);
-
             }
             else
             {
-                TempData["Error"] = "you already exists";
+                TempData["Error"] = "You already exist";
             }
+
             return Ok();
         }
 
-           
 
 
         private void CreateTenantUser(int tenantId , int userId)
