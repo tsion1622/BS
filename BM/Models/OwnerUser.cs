a4055fa3cad1625c4f7a33d9979b04a8a0831dd0
Tsion Tamirat <tayu.16.enye@gmail.com>
2024-12-25 00:46:10 +0300

    created new table owneruse

---
+++ BM/Models/OwnerUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class OwnerUser
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int OwnerId { get; set; }
+
+    public int UserId { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [ForeignKey("OwnerId")]
+    [InverseProperty("OwnerUsers")]
+    public virtual Owner Owner { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("OwnerUsers")]
+    public virtual User User { get; set; }
+}
