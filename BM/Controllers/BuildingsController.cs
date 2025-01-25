c8bf50fbb5ad815bce5efbde52fddbcc93c1d15b
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-25 22:20:36 +0300

    create ,edit and delete buildings and also create floors,rooms foreach corresponding buildings

--- BM/Controllers/BuildingsController.cs
+++ BM/Controllers/BuildingsController.cs
@@ -21,8 +21,24 @@ namespace BM.Controllers
         // GET: Buildings
         public async Task<IActionResult> Index()
         {
+            int? userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                ViewData["error"] = "Please login first";
+            }
             var bIMSContext = _context.Buildings.Include(b => b.BuildingType).Include(b => b.City).Include(b => b.Location).Include(b => b.Owner).Include(b => b.OwnershipType).Include(b => b.User).Include(b => b.UseType);
+           // return View(await bIMSContext.ToListAsync());
+            var building = new Building();
+            ViewData["BuildingTypeId"] = new SelectList(_context.BuildingTypes, "Id", "Name", building.BuildingTypeId);
+            ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Name", building.CityId);
+            ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Name", building.LocationId);
+            ViewData["OwnerId"] = new SelectList(_context.Owners.Include(x => x.OwnerUsers).Where(x => x.OwnerUsers.Any(a => a.UserId == userId)), "Id", "FullName", building.OwnerId);
+            ViewData["OwnerShipTypeId"] = new SelectList(_context.OwnershipTypes, "Id", "Name", building.OwnershipTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "FirstName", building.UserId);
+            ViewData["UseTypeId"] = new SelectList(_context.UseTypes, "Id", "Name", building.UseTypeId);
             return View(await bIMSContext.ToListAsync());
+
+
         }
 
         // GET: Buildings/Details/5
@@ -39,6 +55,10 @@ namespace BM.Controllers
                 .Include(b => b.Location)
                 .Include(b => b.Owner)
                 .Include(b => b.OwnershipType)
+                .Include(b => b.Floors).ThenInclude(b => b.Rooms).ThenInclude(b => b.RoomStatus)
+                .Include(b => b.Floors).ThenInclude(b => b.Rooms).ThenInclude(b => b.RoomPrices)
+                .Include(b => b.Floors).ThenInclude(b => b.Rooms).ThenInclude(b => b.User)
+                .Include(b => b.BuildingEmployees).ThenInclude(b => b.EmployeeType)
                 .Include(b => b.User)
                 .Include(b => b.UseType)
                 .FirstOrDefaultAsync(m => m.Id == id);
@@ -361,6 +381,434 @@ namespace BM.Controllers
             await _context.SaveChangesAsync();
 
             return Ok();
+        }
+
+
+
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddEmployee(string Name, int BuildingId, string PhoneNumber, int EmployeeTypeId)
+        {
+
+            int? userId = HttpContext.Session.GetInt32("UserId");
+            if (!userId.HasValue)
+            {
+                TempData["error"] = "User is not authenticated.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var existingEmployee = _context.BuildingEmployees
+                                            .Any(b => b.FullName.ToLower() == Name.ToLower() && b.BuildingId == BuildingId);
+
+            if (existingEmployee)
+            {
+                TempData["info"] = "An Employee with this name already exists on this building.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var buildingEmployee = new BuildingEmployee
+            {
+                FullName = Name,
+                PhoneNumber = PhoneNumber,
+                BuildingId = BuildingId,
+                EmployeeTypeId = EmployeeTypeId,
+                IsActive = true,
+                IsDeleted = false
+            };
+
+            _context.BuildingEmployees.Add(buildingEmployee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Employees", new { id = BuildingId });
+        }
+
+        public IActionResult Employees(int? id)
+        {
+            ViewData["EmployeeTypeId"] = new SelectList(_context.EmployeeTypes, "Id", "Name");
+            var buildings = _context.Buildings
+                .Include(b => b.BuildingEmployees).ThenInclude(b => b.EmployeeType)
+                .FirstOrDefault(x => x.Id == id);
+            return View(buildings);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddBuilding(string Name, int UseTypeId, int UserId, int NumberOfFloor, int BuildingTypeId, int OwnerId, int OwnerShipTypeId, int CityId, int LocationId, string Description)
+        {
+            int max_building = 3;
+
+            int creatingBuildings = getOwnerBuildingCount(OwnerId);
+            if (creatingBuildings >= max_building)
+            {
+                TempData["info"] = "You have reached the maximum";
+                return RedirectToAction(nameof(Index));
+            }
+            int? userId = HttpContext.Session.GetInt32("UserId");
+            if (!userId.HasValue)
+            {
+                TempData["error"] = "User is not authenticated.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Invalid input data.";
+                return View();
+            }
+
+            var existingBuilding = _context.Buildings
+                               .Any(b => b.Name.ToLower() == Name.ToLower() && b.OwnerId == OwnerId);
+
+            if (existingBuilding)
+            {
+                TempData["info"] = "A building with this name already exists at the specified location.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var building = new Building
+            {
+                Name = Name,
+                UseTypeId = UseTypeId,
+                ConstractionYear = DateTime.Now.ToString("yyyy-MM-dd"),
+                NumberOfFloor = NumberOfFloor,
+                BuildingTypeId = BuildingTypeId,
+                OwnerId = OwnerId,
+                OwnershipTypeId = OwnerShipTypeId,
+                CityId = CityId,
+                LocationId = LocationId,
+                Description = Description,
+                UserId = userId.Value,
+                IsActive = true,
+                IsDeleted = false
+            };
+
+            try
+            {
+                _context.Buildings.Add(building);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Building added successfully!";
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occurred while saving: {ex.Message}";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private int getOwnerBuildingCount(int ownerId)
+        {
+            return _context.Buildings.Where(a => a.OwnerId == ownerId).Count();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditFloor(int FloorId, string Name)
+        {
+            var existingFloor = _context.Floors
+                                              .Any(f => f.Name.ToLower() == Name.ToLower());
+
+
+            if (existingFloor == true)
+            {
+                TempData["info"] = "A floor with this name already exists. Please choose a different name.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var floor = await _context.Floors.FindAsync(FloorId);
+
+            if (floor == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                floor.Name = Name;
+                _context.Floors.Update(floor);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!_context.Floors.Any(r => r.Id == FloorId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditBuilding(int BuildingId, string Name, int userId, int UseTypeId, int NumberOfFloor, int BuildingTypeId, int OwnerId, int OwnershipTypeId, int CityId, int LocationId)
+        {
+            int? UserId = HttpContext.Session.GetInt32("UserId");
+            if (!UserId.HasValue)
+            {
+                TempData["error"] = "User is not authenticated.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var existingBuilding = _context.Buildings
+                                            .Any(b => b.Name.ToLower() == Name.ToLower() && b.CityId == CityId && b.LocationId == LocationId);
+
+            if (existingBuilding)
+            {
+                TempData["info"] = "A building with this name already exists at the specified location.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var building = await _context.Buildings.FindAsync(BuildingId);
+
+            if (building == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                building.Name = Name;
+                building.UseTypeId = UseTypeId;
+                building.OwnershipTypeId = OwnershipTypeId;
+                building.BuildingTypeId = BuildingTypeId;
+                building.OwnerId = OwnerId;
+                building.NumberOfFloor = NumberOfFloor;
+                building.CityId = CityId;
+                building.LocationId = LocationId;
+
+                _context.Update(building);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!_context.Buildings.Any(r => r.Id == BuildingId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteBuilding(int BuildingId)
+        {
+            var building = await _context.Buildings.FindAsync(BuildingId);
+
+            if (building == null)
+            {
+                return NotFound();
+            }
+
+            _context.Buildings.Remove(building);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteFloor(int FloorId)
+        {
+            var floor = await _context.Floors.FindAsync(FloorId);
+
+            if (floor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Floors.Remove(floor);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Details", new { id = floor.BuildingId });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteEmployee(int BuildingEmployeeId)
+        {
+            var buildingEmployee = await _context.BuildingEmployees.FindAsync(BuildingEmployeeId);
+            if (buildingEmployee == null)
+            {
+                return NotFound();
+            }
+            buildingEmployee.IsActive = false;
+            buildingEmployee.IsDeleted = true;
+            _context.Update(buildingEmployee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Employees", new { id = buildingEmployee.BuildingId });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteRoom(int RoomId)
+        {
+            var room = await _context.Rooms.FindAsync(RoomId);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditRoom(int RoomId, string Name, float Width, float Length, string Description)
+        {
+            var existingFloor = _context.Rooms
+                                              .Any(f => f.Name.ToLower() == Name.ToLower());
+
+
+            if (existingFloor == true)
+            {
+                TempData["info"] = "A Room with this name already exists. Please choose a different name.";
+                return RedirectToAction(nameof(Index));
+            }
+            var room = await _context.Rooms.FindAsync(RoomId);
+
+            if (room == null)
+            {
+                return NotFound();
+            }
+            room.Name = Name;
+
+            room.Width = Width;
+            room.Length = Length;
+            room.Description = Description;
+
+            try
+            {
+                _context.Rooms.Update(room);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!_context.Rooms.Any(r => r.Id == RoomId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddFloor(string Name, string NumberOfRoom, int BuildingId)
+        {
+
+            var existingFloor = _context.Floors
+                                              .Any(f => f.BuildingId == BuildingId && f.Name.ToLower() == Name.ToLower());
+
+
+
+            if (existingFloor == true)
+            {
+                TempData["info"] = "A floor with this name already exists. Please choose a different name.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var floor = new Floor
+            {
+                Name = Name,
+                NumberOfRoom = NumberOfRoom,
+                BuildingId = BuildingId
+            };
+
+            _context.Floors.Add(floor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRoom(string Name, int UserId, int FloorId, int RoomStatusId, int OwnerId, float Width, float Length, string Description)
+        {
+
+            int? userId = HttpContext.Session.GetInt32("UserId");
+            var existingFloor = _context.Rooms
+                                             .Any(f => f.FloorId == FloorId && f.Name.ToLower() == Name.ToLower());
+
+
+
+            if (existingFloor == true)
+            {
+                TempData["info"] = "A Room with this name already exists. Please choose a different name.";
+                return RedirectToAction(nameof(Index));
+            }
+            var room = new Room
+            {
+                Name = Name,
+                FloorId = FloorId,
+                RoomStatusId = 1,
+                Width = Width,
+                Length = Length,
+                Description = Description
+            };
+            if (userId.HasValue)
+            {
+                room.UserId = userId.Value;
+            }
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRoomPrice(int RoomId, double price)
+        {
+            int? userId = HttpContext.Session.GetInt32("userid");
+
+            var roomPrice = new RoomPrice
+            {
+                PricePerM2 = price,
+                RoomId = RoomId,
+                AppliedDate = DateOnly.FromDateTime(DateTime.Now),
+                IsActive = true,
+                IsDeleted = false
+
+            };
+            _context.RoomPrices.Add(roomPrice);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
     }
