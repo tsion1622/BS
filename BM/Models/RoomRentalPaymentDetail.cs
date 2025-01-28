72687d33657543521aa2cc12b1a0743c5d4ed84b
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-28 20:41:08 +0300

    holds payment detail information

---
+++ BM/Models/RoomRentalPaymentDetail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class RoomRentalPaymentDetail
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int RoomRentalPaymentId { get; set; }
+
+    public int MonthId { get; set; }
+
+    public int YearId { get; set; }
+
+    public double TotalAmount { get; set; }
+
+    public int AccceptedBy { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime Date { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [ForeignKey("AccceptedBy")]
+    [InverseProperty("RoomRentalPaymentDetails")]
+    public virtual User AccceptedByNavigation { get; set; }
+
+    [ForeignKey("MonthId")]
+    [InverseProperty("RoomRentalPaymentDetails")]
+    public virtual Month Month { get; set; }
+
+    [ForeignKey("RoomRentalPaymentId")]
+    [InverseProperty("RoomRentalPaymentDetails")]
+    public virtual RoomRentalPayment RoomRentalPayment { get; set; }
+
+    [ForeignKey("YearId")]
+    [InverseProperty("RoomRentalPaymentDetails")]
+    public virtual Year Year { get; set; }
+}
