3a0ff32073e53d47ac952cbb4ce30c32a127e49e
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-28 20:29:06 +0300

    detail information about the request

---
+++ BM/Models/ShopSpecification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class ShopSpecification
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    public int ShopRequestId { get; set; }
+
+    public int UseTypeId { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [ForeignKey("ShopRequestId")]
+    [InverseProperty("ShopSpecifications")]
+    public virtual ShopRequest ShopRequest { get; set; }
+
+    [ForeignKey("UseTypeId")]
+    [InverseProperty("ShopSpecifications")]
+    public virtual UseType UseType { get; set; }
+}
