d4ac0ce7fcb7199b61dca204e27e7f0a06872665
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-28 20:42:49 +0300

    manager,casher

---
+++ BM/Models/EmployeeType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class EmployeeType
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [InverseProperty("EmployeeType")]
+    public virtual ICollection<BuildingEmployee> BuildingEmployees { get; set; } = new List<BuildingEmployee>();
+}
