c69d2b037efdca0ebe68a4b16636186067478fda
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-28 20:59:14 +0300

    like shoprequest users must send building request first

---
+++ BM/Controllers/ItemCategoriesController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BM.Models;
+
+namespace BM.Controllers
+{
+    public class ItemCategoriesController : Controller
+    {
+        private readonly BIMSContext _context;
+
+        public ItemCategoriesController(BIMSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ItemCategories
+        public async Task<IActionResult> Index()
+        {
+            var bIMSContext = _context.ItemCategories.Include(i => i.User);
+            return View(await bIMSContext.ToListAsync());
+        }
+
+        // GET: ItemCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemCategory = await _context.ItemCategories
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (itemCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemCategory);
+        }
+
+        // GET: ItemCategories/Create
+        public IActionResult Create()
+        {
+            //ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: ItemCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        //public async Task<IActionResult> Create([Bind("Id,Name,UserId,IsActive,IsDeleted")] ItemCategory itemCategory)
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,UserId,IsActive,IsDeleted")] ItemCategory itemCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                int? sessionUserId = HttpContext.Session.GetInt32("UserId");
+
+                if (sessionUserId.HasValue)
+                {
+                   
+                    itemCategory.UserId = sessionUserId.Value;
+
+                    
+                    var userExists = await _context.Users.AnyAsync(u => u.Id == itemCategory.UserId);
+                    if (!userExists)
+                    {
+                        
+                        ModelState.AddModelError("UserId", "The selected user does not exist.");
+                        
+                        return View(itemCategory);
+                    }
+
+                    
+                    _context.Add(itemCategory);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Account"); 
+                }
+            }
+
+            
+            // ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", itemCategory.UserId);
+            return View(itemCategory);
+        }
+
+        // GET: ItemCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemCategory = await _context.ItemCategories.FindAsync(id);
+            if (itemCategory == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", itemCategory.UserId);
+            return View(itemCategory);
+        }
+
+        // POST: ItemCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,UserId,IsActive,IsDeleted")] ItemCategory itemCategory)
+        {
+            if (id != itemCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(itemCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemCategoryExists(itemCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", itemCategory.UserId);
+            return View(itemCategory);
+        }
+
+        // GET: ItemCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemCategory = await _context.ItemCategories
+                .Include(i => i.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (itemCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemCategory);
+        }
+
+        // POST: ItemCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var itemCategory = await _context.ItemCategories.FindAsync(id);
+            if (itemCategory != null)
+            {
+                _context.ItemCategories.Remove(itemCategory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemCategoryExists(int id)
+        {
+            return _context.ItemCategories.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BM/Models/BuildingRequest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class BuildingRequest
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Description { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string Amount { get; set; }
+
+    public int LocationId { get; set; }
+
+    public int BuildingTypeId { get; set; }
+
+    public int RequestStatusId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime RequestedDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [ForeignKey("BuildingTypeId")]
+    [InverseProperty("BuildingRequests")]
+    public virtual BuildingType BuildingType { get; set; }
+
+    [ForeignKey("LocationId")]
+    [InverseProperty("BuildingRequests")]
+    public virtual Location Location { get; set; }
+
+    [ForeignKey("RequestStatusId")]
+    [InverseProperty("BuildingRequests")]
+    public virtual BuildingRequestStatus RequestStatus { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("BuildingRequests")]
+    public virtual User User { get; set; }
+}
