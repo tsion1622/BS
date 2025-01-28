bae6f7c8cd8a72a30a808116b7020c123e380a7f
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-28 20:19:58 +0300

    for payment information

---
+++ BM/Models/Year.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class Year
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [InverseProperty("Year")]
+    public virtual ICollection<RoomRentalPaymentDetail> RoomRentalPaymentDetails { get; set; } = new List<RoomRentalPaymentDetail>();
+}
