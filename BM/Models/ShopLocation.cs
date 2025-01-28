f12a6131398d78b6aaa6132a5e32d7cdc15a6190
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-28 20:46:36 +0300

    roomName

--- BM/Models/ShopLocation.cs
+++ BM/Models/ShopLocation.cs
@@ -11,35 +11,22 @@ public partial class ShopLocation
     [Key]
     public int Id { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string Name { get; set; }
-
-    public int? LocationId { get; set; }
-
-    public int CityId { get; set; }
-
-    public int BuildingId { get; set; }
+    public int ShopId { get; set; }
 
     public int RoomId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CeratedDate { get; set; }
 
     public bool IsActive { get; set; }
 
     public bool IsDeleted { get; set; }
 
-    [ForeignKey("BuildingId")]
-    [InverseProperty("ShopLocations")]
-    public virtual Building Building { get; set; }
-
-    [ForeignKey("CityId")]
-    [InverseProperty("ShopLocations")]
-    public virtual City City { get; set; }
-
-    [ForeignKey("LocationId")]
-    [InverseProperty("ShopLocations")]
-    public virtual Location Location { get; set; }
-
     [ForeignKey("RoomId")]
     [InverseProperty("ShopLocations")]
     public virtual Room Room { get; set; }
+
+    [ForeignKey("ShopId")]
+    [InverseProperty("ShopLocations")]
+    public virtual Shop Shop { get; set; }
 }
