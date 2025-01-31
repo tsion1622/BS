a3655372cee05a7494fd645cec34580f2d7cf386
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-01-31 23:12:11 +0300

    try to useSignalR for Chat And notification

---
+++ BM/Models/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BM.Models
+{
+    
+    public class ChatHub : Hub
+    {
+       
+            public async Task SendMessage(int receiverId, string message)
+            {
+            await Clients.User(receiverId.ToString()).SendAsync("ReceiveMessage", message);
+         
+        }
+       
+
+
+        public override Task OnConnectedAsync()
+        {
+            string userId = Context.UserIdentifier;
+            Console.WriteLine($"User connected: {userId}");
+            return base.OnConnectedAsync();
+        }
+    }
+
+}
+
