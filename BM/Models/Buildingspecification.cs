105e8aa08bca9dd45ef54dc8a07a40d1a7a9d9f6
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-02-05 21:40:01 +0300

    this model created to hold building request specfication

---
+++ BM/Models/Buildingspecification.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class Buildingspecification
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    public int UseTypeId { get; set; }
+
+    public int NumberOfFloor { get; set; }
+
+    public int CityId { get; set; }
+
+    public int LocationId { get; set; }
+
+    public int BuildingRequestId { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
