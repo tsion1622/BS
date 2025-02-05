accb131846879ef2c4935c199ffc698138c94c7e
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-02-05 21:24:14 +0300

    shopId

---
+++ BM/Models/ShopImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class ShopImage
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ShopId { get; set; }
+
+    [Required]
+    [StringLength(250)]
+    public string ImageUrl { get; set; }
+
+    [Required]
+    [StringLength(150)]
+    public string Description { get; set; }
+
+    public bool? IsProfile { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [ForeignKey("ShopId")]
+    [InverseProperty("ShopImages")]
+    public virtual Shop Shop { get; set; }
+}
