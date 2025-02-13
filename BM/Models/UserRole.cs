bbd5b5fcf4756170b02ee90f606d16f00ac03a69
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-02-13 23:28:00 +0300

    userid,roleid

---
+++ BM/Models/UserRole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class UserRole
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public int RoleId { get; set; }
+
+    public bool IsDefault { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [ForeignKey("RoleId")]
+    [InverseProperty("UserRoles")]
+    public virtual Role Role { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("UserRoles")]
+    public virtual User User { get; set; }
+}
