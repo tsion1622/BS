930e0c32146d2ba849fc8b6f6a03c34be3d3f789
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-15 23:43:14 +0300

    nc

---
+++ BM/Models/ItemEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class ItemEntry
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ShopItemId { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime EntryDate { get; set; }
+
+    public double Quantity { get; set; }
+
+    public double? WithdrawQuantity { get; set; }
+
+    public double? Price { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [InverseProperty("ItemEntry")]
+    public virtual ICollection<ItemEntryVaration> ItemEntryVarations { get; set; } = new List<ItemEntryVaration>();
+
+    [ForeignKey("ShopItemId")]
+    [InverseProperty("ItemEntries")]
+    public virtual ShopItem ShopItem { get; set; }
+}
