be8d46f3121a8f7ebfaed3a191e58c01fc4087d0
Tsion Tamirat <tayu.16.enye@gmail.com>
2025-05-15 23:44:08 +0300

    nc

---
+++ BM/Models/ShopRequestStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BM.Models;
+
+public partial class ShopRequestStatus
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    [InverseProperty("RequestStatus")]
+    public virtual ICollection<ShopRequest> ShopRequests { get; set; } = new List<ShopRequest>();
+}
